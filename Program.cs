085d772513b8b0d70d9a107de48208c87aaae9b2
Marina <korhovama@mail.ru>
2023-09-18 06:30:32 +0300

    Add RandElements and RandSize

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿Random random = new Random();
+
+int RandSize()
+    {
+        int s = random.Next(5, 10);
+        return s;
+    }
+
+void RandElements(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = random.Next(99, 1000);
+        }
+    }
+
+
+
+int[] arr = new int[RandSize()];
+RandElements(arr);
