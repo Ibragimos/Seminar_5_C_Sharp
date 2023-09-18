35886aaf585e694b78a7957d3069c25e6c05358e
Marina <korhovama@mail.ru>
2023-09-18 07:36:24 +0300

    Add Min

--- Program.cs
+++ Program.cs
@@ -6,12 +6,34 @@
         return s;
     }
 
- void RandElements(int[] arr)
+ void RandElements(double[] arr)
     {
-        for (double i = 0; i < arr.Length; i++)
+        for (int i = 0; i < arr.Length; i++)
         {
-            arr[i] = random.Next(1, 100);
+            arr[i] = Math.Round(1.0 + random.NextDouble() * 49.0, 2);
         }
     }
 
+    void Min(double[] arr)
+    {
+        int min = arr[0];
+        for (int i = 0; i < arr.Length; i++)
+        {
+           System.Console.Write($" {arr[i]} ");
+           if(min < arr[i])
+           {
+            min = arr[i];
+           }
+        }
+        /*System.Console.WriteLine();
+        double max = arr.Max();
+        System.Console.WriteLine(max);
+        double min = arr.Min();
+        System.Console.WriteLine(min);*/
+    }
+
     double[] arr = new double[RandSize()];
+    RandElements(arr);
+    
+    Thread.Sleep(10000);
+    Console.Clear(); 
