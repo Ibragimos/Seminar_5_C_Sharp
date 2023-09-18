8009ef8a7a1d3d28f232986fce982ffb92456108
Marina <korhovama@mail.ru>
2023-09-18 06:23:40 +0300

    Even Numbers

--- Program.cs
+++ Program.cs
@@ -1,21 +1,35 @@
-﻿int RandSize()
-{
-    Random size = new Random();
-    int s = size.Next(5, 20);
-    return s;
-}
+﻿
+Random random = new Random();
 
-void RandElements(int[] arr)
+    int RandSize()
+    {
+        int s = random.Next(10, 20);
+        return s;
+    }
+
+ void RandElements(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = random.Next(99, 1000);
+        }
+    }
+
+void Even_Numbers(int[] arr)
 {
-    Random random = new Random();
+    System.Console.Write("Even numbers: ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        
+        if(arr[i] % 2 == 0)
+        {
+            System.Console.Write($" {arr[i]}");
+        }
+    }
     
-for (int i = 0; i < arr.Length; i++)
-{
-    arr[i] = random.Next(99, 1000);
-}
-
 }
 
 
 int[] arr = new int[RandSize()];
 RandElements(arr);
+Even_Numbers(arr);
