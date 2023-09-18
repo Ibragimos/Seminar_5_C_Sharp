3632201ff9682a802d5f1312749eb7fb185f41f5
Marina <korhovama@mail.ru>
2023-09-18 06:06:51 +0300

    Added a randomizer the number of elements themselves

--- Program.cs
+++ Program.cs
@@ -1,11 +1,21 @@
-﻿int Rand()
+﻿int RandSize()
 {
     Random size = new Random();
     int s = size.Next(5, 20);
     return s;
 }
 
+void RandElements(int[] arr)
+{
+    Random random = new Random();
+    
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = random.Next(99, 1000);
+}
+
+}
 
 
-
-int[] arr = new int[Rand()];
+int[] arr = new int[RandSize()];
+RandElements(arr);
